33af5e4b4eea792c41f1ef1e101ec1136f0a25f1
Howard Pinsley <hpinsley@gmail.com>
2016-03-06 12:05:33 -0500

    Print audit records

--- app.ts
+++ app.ts
@@ -1,9 +1,25 @@
-import * as path from 'path';
+interface IAuditRecord {
+    username: string;
+    time: Date;
+    recType: string;
+    details: any;
+}
 
 let mongoskin = require('mongoskin');   //Using require since there is no tsd file
 let mongo_url = process.env.MONGOLABS_SECURITYDATA_URL
+let db = mongoskin.db(mongo_url, {safe:true});
+let coll = db.collection('audit');
 
-console.log(`Mongo url is ${mongo_url}.`);
+coll.find({}).sort({ time: -1}).toArray((e, records: IAuditRecord[]) => {
+    if (e) {
+        console.log("Error", e);
+    }
+    else {
+        for (let r of records) {
+            let d = JSON.stringify(r.details);
+            console.log(`${r.time}: ${r.username} - ${r.recType}: ${d}`);
 
-var db = mongoskin.db(mongo_url, {safe:true});
-console.log(db);
+        }
+        db.close();
+    }
+});
