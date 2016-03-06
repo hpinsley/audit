fbec42b7c436903e671c2d7ec4c6f0e713e17b96
Howard Pinsley <hpinsley@gmail.com>
2016-03-06 10:52:40 -0500

    Initial commit.  Compiles

---
+++ app.ts
@@ -0,0 +1,8 @@
+import * as path from 'path';
+let mongoskin = require('mongoskin');   //Using require since there is no tsd file
+let mongo_url = process.env.MONGOLABS_SECURITY_URL
+
+console.log(`Mongo url is ${mongo_url}.`);
+
+var db = mongoskin.db(mongo_url, {safe:true});
+console.log(db);
